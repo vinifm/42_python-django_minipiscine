4bdd67b5b6369aa9dacbe64b458f3001675e6ca0
viferrei <vifm@outlook.com>
2023-02-02 11:24:30 -0300

    d01-ex00

--- d01/ex00/var.py
+++ d01/ex00/var.py
@@ -1,6 +1,5 @@
 def my_var():
-	var_list = [42, 42.0, False, [1, 2, 3], (0, 1, 2), {"apple", "banana", "tangerine"},
-				{"Name": "Vini", "Loves to": "sleep"}, "I'm running out of types", type(42)]
+	var_list = [42, "42", "quarante-deux", 42.0, True, [42], {42: 42}, (42,), set()]
 	for var in var_list:
 		print(f"{var} has a type {type(var)}")
 
